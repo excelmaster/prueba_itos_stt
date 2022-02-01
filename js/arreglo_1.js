221e61415fd7a272271df3507d4e0d87b5e74ac8
betfie1@gmail.com <betfie1@gmail.com>
2022-02-01 14:58:41 -0500

    generador de tabla ok

---
+++ js/arreglo_1.js
@@ -0,0 +1,28 @@
+/* calculo año bisiesto  */
+
+$('#btn_tabla').click(function(){
+    $('#error_filas').hide();
+    $('#error_columnas').hide();
+    var filas = $('#filas').val();
+    var columnas = $('#columnas').val();
+    if(filas == ""){$('#error_filas').show(); };
+    if(columnas == ""){$('#error_columnas').show(); };
+
+    var codigo = '<table class="table table-bordered"><thead><tr>';
+    for(var i = 1 ; i<=columnas; i++){
+        codigo += '<th scope="col">Col-' + i + '</th>';
+    };
+
+    codigo += '</tr></thead><tbody>'
+    for(var j = 1 ; j<= filas; j++){
+        codigo += '<tr>';
+            for(var x = 1 ; x<=columnas; x++){
+                codigo += (x==1) ? '<td>' + j + '</td>' : '<td> - </td>';
+            };
+        codigo += '</tr>';
+    }
+
+    $('#tablagenerada').html(codigo);
+    //alert(filas);
+
+});
