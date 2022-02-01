d7e68f0edd7209635a5168d3bf4be3c4b5bc08b8
betfie1@gmail.com <betfie1@gmail.com>
2022-02-01 14:04:06 -0500

    bisiesto ok

--- js/bisiesto.js
+++ js/bisiesto.js
@@ -1,23 +1,22 @@
 /* calculo año bisiesto  */
-$('#comprobar').click(function(){
+
+$('#my').click(function(){
     var y = $('#byear').val();
     if(y != "" ){
-        
         if( (y % 4 == 0 && y % 100 != 0) || y % 400 == 0 ){
-            alert ("si") ;
+            alert ("el año " + y + " ES Bisiesto") ;
             $('#rbisiesto').removeClass('alert-info');
             $('#rbisiesto').addClass('alert-success');
-            $('#rbisiesto').val('el año ' + y + 'es bisiesto');        
+            $('#rbisiesto').text('el año ' + y + ' es bisiesto');
         } else {
-            alert (" no");
+            alert ("El año " + y + " NO ES bisiesto");
             $('#rbisiesto').removeClass('alert-info');
             $('#rbisiesto').addClass('alert-warning');
-            $('#rbisiesto').val('el año ' + y + 'es bisiesto');
+            $('#rbisiesto').text('el año ' + y + ' NO ES bisiesto');
         };
-        
-        $('#resultado').show();
 
+        $( '#rbisiesto' ).fadeIn( 3000 ).delay( 1500 ).fadeOut( 4000 );
+        $('#rbisiesto').addClass('show');
     };
 
-    
 });
