c7cd9f837e0f0ce1cac1400fc59741e717087562
betfie1@gmail.com <betfie1@gmail.com>
2022-02-01 22:50:53 -0500

    ok  conjuntos

--- js/letras.js
+++ js/letras.js
@@ -50,10 +50,25 @@ function arrayLetras(){
     codigo += '</tr></thead>'
     $('#inter').html(codigo);
 
-    //diferencia
-    let array_diff = new Set([...array_a].filter(x => ![...array_b].has(x)));
-    alert(array_diff);
-    
+    // diferencia
+    let array_diff = array_a.filter(value => !array_b.includes(value));
+    var codigo = '<table class="table table-bordered table-responsive"><thead><tr>';
+    for(var i = 0; i<array_diff.length;i++){
+        codigo += '<th scope="col" id="num_' + i + '">' + array_diff[i] + '</th>';
+    }
+    codigo += '</tr></thead>'
+    $('#diff').html(codigo);
+
+    //diferencia simetrica
+    let array_simetrica = array_union.filter( element => !array_inter_unique.includes(element));
+    let array_simetrica_unique = array_simetrica.filter((value, index, self) => self.indexOf(value) === index);
+    alert(array_simetrica_unique);
+    var codigo = '<table class="table table-bordered table-responsive"><thead><tr>';
+    for(var i = 0; i<array_simetrica_unique.length;i++){
+        codigo += '<th scope="col" id="num_' + i + '">' + array_simetrica_unique[i] + '</th>';
+    }
+    codigo += '</tr></thead>'
+    $('#simetrica').html(codigo);
 
 
 }
