0608e12082d7c4ca85120038e6a0afec10dc5fc9
betfie1@gmail.com <betfie1@gmail.com>
2022-02-01 22:31:26 -0500

    union e intersección ok

---
+++ js/letras.js
@@ -0,0 +1,59 @@
+/* calculo año bisiesto  */
+
+function arrayLetras(){
+    //Generación de arrays
+    var grupo = new Array();
+    grupo = ['a','b'];
+    
+    grupo.forEach(element => {
+        var result = [];
+        n = 10; // cantidad de letras a generar
+        for(var i=0;i<n;i++){
+            // Genera un número de 0 a 25
+            var ranNum = Math.ceil(Math.random() * 25);
+            // llamamos un código ASCII entre 25 y 65
+            result.push(String.fromCharCode(65+ranNum));
+        }
+        
+        //render de la tabla
+        var codigo = '<table class="table table-bordered"><thead><tr>';
+        for(var i = 0; i<10;i++){
+            codigo += '<th scope="col" id="num_' + i + '">' + result[i] + '</th>';
+        }
+        codigo += '</tr></thead>'
+        $('#array_' + element).html(codigo);
+        $('#matriz_' + element).text(result.toLocaleString());    
+    });
+
+    let matriz_a = $('#matriz_a').text();
+    array_a= matriz_a.split(',');
+    let matriz_b = $('#matriz_b').text();
+    array_b= matriz_b.split(',');
+
+    //union de conjunto
+    array_union = array_a.concat(array_b);
+
+    var codigo = '<table class="table table-bordered table-responsive"><thead><tr>';
+    for(var i = 0; i<array_union.length;i++){
+        codigo += '<th scope="col" id="num_' + i + '">' + array_union[i] + '</th>';
+    }
+    codigo += '</tr></thead>'
+    $('#union').html(codigo);
+
+    //intersección
+    let array_inter = array_a.filter(value => array_b.includes(value));
+    let array_inter_unique = array_inter.filter((value, index, self) => self.indexOf(value) === index);
+    var codigo = '<table class="table table-bordered table-responsive"><thead><tr>';
+    for(var i = 0; i<array_inter_unique.length;i++){
+        codigo += '<th scope="col" id="num_' + i + '">' + array_inter_unique[i] + '</th>';
+    }
+    codigo += '</tr></thead>'
+    $('#inter').html(codigo);
+
+    //diferencia
+    let array_diff = new Set([...array_a].filter(x => ![...array_b].has(x)));
+    alert(array_diff);
+    
+
+
+}
